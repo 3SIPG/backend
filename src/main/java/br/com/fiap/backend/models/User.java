172c1e4012810833b9111bf14f67d594a39890e2
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-05-03 13:47:15 -0300

    project: user model

--- src/main/java/br/com/fiap/backend/models/User.java
+++ src/main/java/br/com/fiap/backend/models/User.java
@@ -1,5 +1,57 @@
 package br.com.fiap.backend.models;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Objects;
+
+@Table(name = "tb_users")
+@Entity(name = "User")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    public User(String senha, String nome, String email) {
+        this.senha = senha;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(nome, user.nome) && Objects.equals(email, user.email) && Objects.equals(senha, user.senha);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, senha);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
 }
