0f3e2bb8160cc9a4f09818b4d627f2a3f666302d
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-05-04 07:33:49 -0300

    project: import sqç

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,8 @@
-INSERT INTO tb_users(nome,email,senha) VALUES('vitor', 'email@email.com','senha');
-INSERT INTO tb_users(nome,email,senha) VALUES('enzo', 'email@email.com','senha');
-INSERT INTO tb_users(nome,email,senha) VALUES('leo', 'email@email.com','senha');
-INSERT INTO tb_users(nome,email,senha) VALUES('vitinho', 'email@email.com','senha');
+INSERT INTO tb_trainings(title) VALUES ('Backend');
+INSERT INTO tb_trainings(title) VALUES ('Frontend');
+INSERT INTO tb_trainings(title) VALUES ('UX');
+
+INSERT INTO tb_users(name,email,password) VALUES('vitor', 'email@email.com','senha');
+INSERT INTO tb_users(name,email,password) VALUES('enzo', 'email@email.com','senha');
+INSERT INTO tb_users(name,email,password) VALUES('leo', 'email@email.com','senha');
+INSERT INTO tb_users(name,email,password) VALUES('vitinho', 'email@email.com','senha');
