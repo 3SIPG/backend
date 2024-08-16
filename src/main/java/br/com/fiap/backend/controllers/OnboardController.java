098fee9d1be525dc3e7bad5cde9d25fc3ff384ef
Leonardo Cardoso <leonardorecco3@gmail.com>
2024-08-15 22:55:52 -0300

    feat: cors

--- src/main/java/br/com/fiap/backend/controllers/OnboardController.java
+++ src/main/java/br/com/fiap/backend/controllers/OnboardController.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("onboard")
 public class OnboardController {
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,6 +7,6 @@ INSERT INTO tb_onboard(title) VALUES ('UX');
 -- INSERT INTO tb_users(name,email,password) VALUES('leo', 'email@email.com','senha');
 -- INSERT INTO tb_users(name,email,password) VALUES('vitinho', 'email@email.com','senha');
 
-INSERT INTO tb_videos(title,url,training_id, banner) VALUES('Java', 'https://www.youtube.com/watch?v=EpXYPB1rv4w',1,'https://www.youtube.com/watch?v=EpXYPB1rv4w');
-INSERT INTO tb_videos(title,url,training_id, banner) VALUES('C#', 'https://www.youtube.com/watch?v=mUMvW6JHzk4',1,'https://www.youtube.com/watch?v=EpXYPB1rv4w');
-INSERT INTO tb_videos(title,url,training_id, banner) VALUES('React', 'https://www.youtube.com/watch?v=NhUr8cwDiiM',2,'https://www.youtube.com/watch?v=EpXYPB1rv4w');
+INSERT INTO tb_videos(title,url,training_id, banner) VALUES('Java', 'https://www.youtube.com/watch?v=EpXYPB1rv4w',1,'https://i.ytimg.com/vi/Iqr3XIhSnUQ/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOmwb05sM1htIKLnpvbvx93MyzCA');
+INSERT INTO tb_videos(title,url,training_id, banner) VALUES('C#', 'https://www.youtube.com/watch?v=mUMvW6JHzk4',1,'https://i.ytimg.com/vi/Iqr3XIhSnUQ/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOmwb05sM1htIKLnpvbvx93MyzCA');
+INSERT INTO tb_videos(title,url,training_id, banner) VALUES('React', 'https://www.youtube.com/watch?v=NhUr8cwDiiM',2,'https://i.ytimg.com/vi/Iqr3XIhSnUQ/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOmwb05sM1htIKLnpvbvx93MyzCA');
